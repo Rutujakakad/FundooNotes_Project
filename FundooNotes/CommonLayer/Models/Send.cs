6158c77d51a203cca0eac7e0a784e8305e8106e8
67418_Rutuja <rutubkakad@gmail.com>
2025-02-05 07:42:13 +0530

    Specified the models

---
+++ FundooNotes/CommonLayer/Models/Send.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace CommonLayer.Models
+{
+    public class Send
+    {
+        public string ToEmail {  get; set; }
+        public string Token {  get; set; }
+
+        public string SendMail(string ToEMail, string Token)
+        {
+            string FromEmail = "demomail@gmail.com";
+            MailMessage message = new MailMessage(FromEmail, ToEmail);
+            string MailBody = "The token for the password: " + Token;
+            message.Subject = "Token generated for resetting password";
+            message.Body = MailBody.ToString();
+            message.BodyEncoding = Encoding.UTF8;
+            message.IsBodyHtml = true;
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            NetworkCredential credential = new NetworkCredential("demomail@gmail.com", "nygq vnsu nmirqooc");
+
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = credential;
+
+            smtpClient.Send(message);
+            return ToEMail;
+        }
+    }
+}
