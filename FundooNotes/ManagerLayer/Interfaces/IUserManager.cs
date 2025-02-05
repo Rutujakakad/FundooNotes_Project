d4c795116964c04e89ebef5ca824a837778c8b4c
67418_Rutuja <rutubkakad@gmail.com>
2025-02-05 07:53:45 +0530

    add manager layer for business logic handling

---
+++ FundooNotes/ManagerLayer/Interfaces/IUserManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using RepositoryLayer.Entity;
+
+namespace ManagerLayer.Interfaces
+{
+    public interface IUserManager
+    {
+        
+        public UserEntity Registration(RegisterModel model);
+        public bool MailExist(string email);
+        public string LoginEmailPassword(string email, string password);
+        public string Login(LoginModel login);
+
+        public ForgotPasswordModel ForgotPassword(string email);
+        
+    }
+}
---
+++ FundooNotes/ManagerLayer/Services/UserManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonLayer.Models;
+using ManagerLayer.Interfaces;
+using RepositoryLayer.Context;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interfaces;
+
+namespace ManagerLayer.Services
+{
+    public class UserManager : IUserManager
+    {
+        private readonly IUserRepository user;
+        private readonly FundooDBContext context;
+        public UserManager(IUserRepository user, FundooDBContext context)
+        {
+            this.user = user;
+            this.context = context;
+        }
+        public UserEntity Registration(RegisterModel model)
+        {
+            return user.Registration(model);
+        }
+
+        public bool MailExist(string email)
+        {
+            return user.MailExist(email);
+        }
+
+        public string LoginEmailPassword(string email, string password)
+        {
+            return user.LoginEmailPassword(email, password);
+        }
+
+        public string Login(LoginModel login)
+        {
+            return user.Login(login);
+        }
+
+        public ForgotPasswordModel ForgotPassword(string email)
+        {
+            return user.ForgotPassword(email);
+        }
+    }
+}
